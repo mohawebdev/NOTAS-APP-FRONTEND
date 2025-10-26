56de8168e3905028b50e04dde1d20221ed3220a7
Mohawebdev <mohawebmaghzaz@gmail.com>
2025-10-26 22:14:18 +0100

    Agregamos implementaciones de delete y update

--- src/components/CardNote.jsx
+++ src/components/CardNote.jsx
@@ -1,25 +1,80 @@
 import { SquarePen, Trash } from "lucide-react";
+import DeleteConfirmationModal from "./DeleteConfirmationModal.jsx";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {toast}  from 'react-toastify';
+import axios from 'axios';
 
 // id
-const CardNote = ({ title, description, date }) => {
+const CardNote = ({ title, description, date, id, onDelete }) => {
+  const navigate = useNavigate();
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const deleteNote= async()=>{
+    try {
+      //Eliminando la nota de la BD
+      await axios.delete(`${import.meta.env.VITE_API_URL}/api/notes/${id}`)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error('Error al eliminar la nota');
+        }
+        toast.success('Nota eliminada correctamente', {
+            position: 'bottom-right',
+            autoClose: 3000,
+            theme: 'colored',
+          });
+        
+
+        //Eliminamos la nota desde el frontend desde la UI
+        if (onDelete)  onDelete(id);
+        
+        //Cerrar el modal
+        setShowConfirmModal(false);
+
+        //Refrescar la página después de eliminar
+        window.location.reload();
+      });
+    } catch (error) {
+      console.log(error);
+      toast.error('Error al eliminar la nota', {
+        position: 'bottom-right',
+        autoClose: 3000,
+        theme: 'colored',
+      });
+    }
+  };
+
   return (
-    <div className="card bg-base-300 w-full shadow-sm">
-      <div className="card-body">
-        <h2 className="card-title text-accent font-bold lg:text-2xl">
-          {title}
-        </h2>
-        <p className="text-amber-50">
-          {description}
-        </p>
-        <div className="flex justify-between items-center mt-6">
-          <time dateTime={date}>{date}</time>
-          <div className="flex gap-4">
-            <SquarePen className="text-white cursor-pointer"></SquarePen>
-            <Trash className="text-red-400 cursor-pointer"></Trash>
+    <>
+      <div className="card bg-base-300 w-full shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title text-accent font-bold lg:text-2xl">
+            {title}
+          </h2>
+          <p className="text-amber-50">{description}</p>
+          <div className="flex justify-between items-center mt-6">
+            <time dateTime={date}>{date}</time>
+            <div className="flex gap-4">
+              <SquarePen
+                className="text-white cursor-pointer"
+                onClick={() => navigate(`/editNote/${id}`)}
+              ></SquarePen>
+              <Trash
+                className="text-red-400 cursor-pointer"
+                onClick={() => setShowConfirmModal(true)}
+              ></Trash>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {showConfirmModal && (
+        <DeleteConfirmationModal
+         title={title}
+          deleteNote={deleteNote}
+          setShowConfirmModal={setShowConfirmModal}
+        />
+      )}
+    </>
   );
 };
 export default CardNote;
--- src/pages/EditNotePage.jsx
+++ src/pages/EditNotePage.jsx
@@ -1,7 +1,54 @@
+import NoteForm from '../components/NoteForm';
+import axios from 'axios';
+import {toast} from 'react-toastify';
+import {useNavigate, useParams} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+
+const apiURL= import.meta.env.VITE_API_URL;
+
 
 const EditNotePage = () => {
+
+const { id } = useParams();
+const navigate= useNavigate();
+const [initialData, setInitialData]= useState({
+  title: "",
+  description: "",
+});
+
+useEffect(()=>{
+  axios.get(`${apiURL}/api/notes/${id}`).then((res)=>{
+    setInitialData({
+      title: res.data.title,
+      description: res.data.description,
+    });
+  });
+}, [id]);
+
+const handleUpdate= async(note)=>{
+  await axios.put(`${apiURL}/api/notes/${id}`, note).then((res)=>{
+    if(res.status === 200){
+      toast.success('¡Nota actualizada con éxito!', {
+        position: 'bottom-right',
+        autoClose: 3000,
+        theme: 'colored',
+      });
+      navigate('/');
+    }else{
+      toast.error("Error al actualizar la nota", {
+          position: "bottom-right",
+          autoClose: 3000,
+          theme: "colored",
+        });
+    }
+  })
+}
+
   return (
-    <div>EditNotePage</div>
+    <div>
+      <h1 className='text-5xl font-bold text-center mb-8'>Editar Nota</h1>
+<NoteForm initialData={initialData} onSubmit={handleUpdate} />
+    </div>
   )
 }
 export default EditNotePage;
